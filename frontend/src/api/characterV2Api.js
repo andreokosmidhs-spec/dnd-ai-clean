2411dd861e30b33890086baca7a1a1140c629198
andreo kosmidhs <andreo.kosmidhs@gmail.com>
2025-12-09 01:19:53 +0000

    Message (Ctrl+Enter to commit on "main")

--- frontend/src/api/characterV2Api.js
+++ frontend/src/api/characterV2Api.js
@@ -1,7 +1,13 @@
-// API function for creating V2 characters
-import apiClient from '../lib/apiClient';
+import axios from './axiosConfig';
 
+// Create a new character using the V2 schema
 export const createCharacterV2 = async (characterData) => {
-  const response = await apiClient.post('/api/characters_v2/create', characterData);
-  return response.data;
+  const res = await axios.post('/api/characters_v2/create', characterData);
+  return res.data;
+};
+
+// Get all V2 characters (temporary debug endpoint)
+export const getCharactersV2 = async () => {
+  const res = await axios.get('/api/characters_v2');
+  return res.data;
 };
